dbed6353c08339fa959351a8e7b23f30649b0ae0
yakogdan <yakogdan@gmail.com>
2022-12-16 16:35:48 +0800

    Вёрстка экрана деталей фильма.

--- app/src/main/java/com/yakogdan/mtsteta/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/yakogdan/mtsteta/presentation/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package com.yakogdan.mtsteta.presentation.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yakogdan.mtsteta.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
